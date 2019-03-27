6b831409d9241af115d1df625a80a6a06d788e21
mtangaro <ma.tangaro@gmail.com>
2019-03-27 09:38:14 +0000

    add install scripts

---
+++ fgw_install_scripts/setup_OCCI.sh
@@ -0,0 +1,361 @@
+#!/bin/bash
+#
+# Setup script for the FutureGateway portal
+#
+# 17.09.2015 - riccardo.bruno@ct.infn.it
+#
+
+# Load commons setup environment variables
+. setup_config.sh common
+
+#
+# Setup environment variables (default values)
+#
+USEFEDCLOUD=1                                     # Set to 1 for FedCloud setup script
+
+# This file contains common variables for setup_* scripts it may be used to override above settings
+. setup_config.sh
+
+# pre installation steps
+preinstall_occi() {
+  # 1st ensure that this script executes as su/root priviledges
+  if [ $(id -u) != 0 ]; then
+    echo "Please execute this script with sudo or as a root user"
+    exit 1
+  fi
+  # FGENV
+  if [ "${FGENV}" = "" ]; then
+    echo "FATAL: \$FGENV environment variable must be set; please refer to the setup_FGPortal.sh script"
+    return 1
+  fi
+  if [ ! -f $FGENV ]; then
+    echo "FATAL: Unable to locate FutureGateway' setenv.sh environment file; please refer to the setup_FGPortal.sh script"
+    return 1
+  fi
+  # sudo/root users do not have the FutureGateway environment
+  source $FGENV
+  # RUNDIR
+  if [ "${RUNDIR}" = "" ]; then
+    echo "FATAL: \$RUNDIR environment variable must be set; please refer to the setup_FGPortal.sh script"
+    return 1
+  fi
+  if [ ! -d $RUNDIR ]; then
+    echo "FATAL: Unable to locate directory from where setup_FGPortal.sh script has been executed"
+    return 1
+  fi
+  # FGSETUP
+  if [ "${FGSETUP}" = "" ]; then
+    echo "FATAL: \$FGSETUP environment variable must be set; please refer to the setup_FGPortal.sh script"
+    return 1
+  fi
+  if [ ! -f $FGSETUP ]; then
+    echo "FATAL: Unable to locate setup tracking file for setup_FGPortal.sh script"
+    return 1
+  fi
+  # FGLOCATION
+  if [ "${FGLOCATION}" = "" ]; then
+    echo "FATAL: \$FGSETUP environment variable must be set; please refer to the setup_FGPortal.sh script"
+    return 1
+  fi
+  if [ ! -d $FGLOCATION ]; then
+    echo "FATAL: Unable to locate FutureGateway installation directory; please refer to the setup_FGPortal.sh script"
+    return 1
+  fi
+  # JAVA_HOME
+  if [ "${JAVA_HOME}" = "" ]; then
+    echo "FATAL: \$JAVA_HOME environment variable must be set; please refer to the setenv.sh environment file"
+    return 1
+  fi
+  if [ ! -d $JAVA_HOME ]; then
+    echo "FATAL: Unable to locate Java' home directory, please refer to your JAVA installation settings"
+    return 1
+  fi
+  # Determine JAVA version
+  JVER=$(java -version 2>&1 | awk -F'"' '{ print $2 }' | awk -F"_" '{ print $1 }' | sed s/'\.'//g)
+  if [ "${JVER}" = "" ]; then
+    echo "FATAL: Unrecognized java version; is it installed in your system?"
+    return 1
+  fi 
+  SYSTEM=$(uname -s)
+  if [ $SYSTEM = "Darwin" ]; then
+    if [ "${BREW}" = "" ]; then
+      echo "FATAL: brew is not present in your system; unable install"
+      return 1
+    fi
+  elif [ $SYSTEM="Linux" ]; then    
+    if [ "${APTGET}" = "" -a "${YUM}" = "" ]; then
+      echo "FATAL: No supported installation facility found in your system (apt-get|yum); unable install"
+      return 1
+    fi
+  fi
+  # if YUM check for EPEL (mandatory)
+  if [ "${YUM}" != "" ]; then
+    EPEL=$(ls -1 /etc/yum.repos.d/*.repo | grep -i epel | wc -l)
+    if [ $EPEL -eq 0 ]; then
+      echo "FATAL: You need to install EPEL repo first: 'yum install epel-release # on CentOS7'"
+      return 1
+    fi  
+  fi
+  CURRDIR=$PWD
+  # From now on the each installation phase assumes that the 
+  # current directory is $FGLOCATION; during the installation
+  # the current directory could be changed but always included
+  # into a: cd .../newpath; cd - block statements
+  # RUNDIR will contain the directory path at the setup execution
+  # time
+  cd $FGLOCATION
+}
+
+# installing GSI
+install_gsi() {
+  if [ -e $RUNDIR/.fgSetup ]; then
+    SETUPCHK=$(cat $RUNDIR/.fgSetup | grep "gsi")
+  else
+    SETUPCHK=""
+  fi
+  if [ "${SETUPCHK}" != "" ]; then
+    echo "GSI seems already installed; skipping this phase"
+    return 0
+  fi  
+  if [ $USEFEDCLOUD -ne 0 ]; then
+    # No need to install now GSI it will be done by FedCloud script
+    echo "GSI support will be isntalled later with FedCloud setup"
+  else
+    # Install setup script GSI
+    echo "GSI support will be isntalled ..."
+    if [ "${BREW}" != "" ]; then
+      $FGREPO/lcg_CA.sh
+      get_file https://dist.eugridpma.info/distribution/util/fetch-crl/fetch-crl-3.0.16.tar.gz
+      tar xvfz fetch-crl-3.0.16.tar.gz
+      cd fetch-crl-3.0.16
+      make install
+      cd -
+      fetch-crl
+      su - $FGUSER -c "${BREW} install voms"
+    elif [ "${APTGET}" != "" ]; then
+      wget -q -O - https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | apt-key add -
+      cat >> /etc/apt/sources.list <<EOF
+# EGI Thrustanchors
+deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core
+EOF
+      $APTGET update
+      $APTGET install -y ca-policy-egi-core
+      $APTGET install -y libwww-perl
+      get_file https://dist.eugridpma.info/distribution/util/fetch-crl/fetch-crl-3.0.16.tar.gz
+      tar xvfz fetch-crl-3.0.16.tar.gz
+      cd fetch-crl-3.0.16
+      make install
+      cd -
+      rm -rf fetch-crl-3.0.16
+      rm -f fetch-crl-3.0.16.tar.gz
+      fetch-crl
+      $APTGET install -y voms-clients
+    elif [ "${YUM}" != "" ]; then
+      cat > /etc/yum.repos.d/EGI-trustanchors.repo <<EOF
+[EGI-trustanchors]
+name=EGI-trustanchors
+baseurl=http://repository.egi.eu/sw/production/cas/1/current/
+gpgkey=http://repository.egi.eu/sw/production/cas/1/GPG-KEY-EUGridPMA-RPM-3
+gpgcheck=1
+enabled=1
+EOF
+      $YUM install -y ca-policy-egi-core
+      $YUM install -y fetch-crl
+      fetch-crl
+      $YUM install -y voms-clients
+    else
+      echo "FATAL: Unsupported system: $SYSTEM"
+      return 1
+    fi
+  fi
+  # report to .fgSetup to track success
+  get_ts
+  echo "$TS   gsi" >> $RUNDIR/.fgSetup
+  return 0
+}
+
+# install OCCI Client interface
+install_occi() {
+  if [ -e $RUNDIR/.fgSetup ]; then
+    SETUPCHK=$(cat $RUNDIR/.fgSetup | grep "occi")
+  else
+    SETUPCHK=""
+  fi
+  if [ "${SETUPCHK}" != "" ]; then
+    echo "OCCI seems already installed; skipping this phase"
+    return 0
+  fi
+  echo "Installing OCCI ..."
+  if [ $USEFEDCLOUD -ne 0 ]; then
+    # Using FedCloud installation (USEFEDCLOUD != 0)
+    echo "Using FedCloud setup"
+    # This installation supports MacOSX, Debian and ELx
+    curl -L http://go.egi.eu/fedcloud.ui | /bin/bash -
+    # Now configure VO fedcloud.egi.eu
+    mkdir -p /etc/grid-security/vomsdir/fedcloud.egi.eu
+
+    cat > /etc/grid-security/vomsdir/fedcloud.egi.eu/voms1.egee.cesnet.cz.lsc << EOF 
+/DC=org/DC=terena/DC=tcs/OU=Domain Control Validated/CN=voms1.egee.cesnet.cz
+/C=NL/O=TERENA/CN=TERENA eScience SSL CA
+EOF
+    cat > /etc/grid-security/vomsdir/fedcloud.egi.eu/voms2.grid.cesnet.cz << EOF 
+/DC=org/DC=terena/DC=tcs/C=CZ/ST=Hlavni mesto Praha/L=Praha 6/O=CESNET/CN=voms2.grid.cesnet.cz
+/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3
+EOF
+    cat >> /etc/vomses/fedcloud.egi.eu << EOF 
+"fedcloud.egi.eu" "voms1.egee.cesnet.cz" "15002" "/DC=org/DC=terena/DC=tcs/OU=Domain Control Validated/CN=voms1.egee.cesnet.cz" "fedcloud.egi.eu" "24"
+"fedcloud.egi.eu" "voms2.grid.cesnet.cz" "15002" "/DC=org/DC=terena/DC=tcs/C=CZ/ST=Hlavni mesto Praha/L=Praha 6/O=CESNET/CN=voms2.grid.cesnet.cz" "fedcloud.egi.eu" "24"
+EOF
+    # Update CLRs
+    fetch-crl
+  else
+    echo "Using original FG setup"
+    # installation foresees a different process for each supported architecture
+    if [ $SYSTEM = "Darwin" ]; then
+      # MacOS X
+      RUBY=$(which ruby)
+      if [ "${RUBY}" = "" ]; then
+          $BREW isntall ruby
+          RES=$?
+          if [ $RES -ne 0 ]; then
+              echo "FATAL: Your OCCI-client could not be installed."
+              echo "       Please try to fix your ruby/gem environment first."
+              return 1
+          fi
+      fi
+      GEM=$(which gem)
+      if [ "${GEM}" = "" ]; then
+          echo "FATAL: Unhespected ruby installation without gem"
+          echo "       Please try to fix your environment first."
+          return 1    
+      fi
+      $GEM install occi-cli
+      RES=$?
+      if [ $RES -ne 0 ]; then
+          echo "FATAL: Your OCCI-client could not be installed."
+          echo "       Please try to fix your ruby/gem environment first."
+      fi
+    elif [ $SYSTEM="Linux" ]; then
+      if [ "${APTGET}" != "" ]; then
+         # Debian system
+         RUBY=$(which ruby)
+         if [ "${RUBY}" = "" ]; then
+             $APTGET isntall ruby
+             RES=$?
+             if [ $RES -ne 0 ]; then
+                 echo "FATAL: Your OCCI-client could not be installed."
+                 echo "       Please try to fix your ruby/gem environment first."
+                 return 1
+             fi
+         fi
+         GEM=$(which gem)
+         if [ "${GEM}" = "" ]; then
+             echo "FATAL: Unhespected ruby installation without gem"
+             echo "       Please try to fix your environment first."
+             return 1    
+         fi
+         $GEM install occi-cli
+         RES=$?
+         if [ $RES -ne 0 ]; then
+             echo "FATAL: Your OCCI-client could not be installed."
+             echo "       Please try to fix your ruby/gem environment first."
+         fi
+      elif [ "${YUM}" != "" ]; then
+         # Enterprise Linux System
+         get_file http://repository.egi.eu/community/software/rocci.cli/4.2.x/releases/repofiles/sl-6-x86_64.repo /etc/yum.repos.d rocci-cli.repo
+         yum install -y occi-cli
+         ln -s /opt/occi-cli/bin/occi /usr/bin/occi
+      else
+        echo "FATAL: No supported installation facility found in your system (apt-get|yum); unable install"
+        return 1
+      fi
+    fi
+  fi
+  # report to .fgSetup to track success    
+  get_ts
+  echo "$TS  occi" >> $RUNDIR/.fgSetup
+}
+
+# install additional VOMSES
+install_vomses() {
+  if [ -e $RUNDIR/.fgSetup ]; then
+    SETUPCHK=$(cat $RUNDIR/.fgSetup | grep "vomses")
+  else
+    SETUPCHK=""
+  fi
+  if [ "${SETUPCHK}" != "" ]; then
+    echo "OCCI seems already installed; skipping this phase"
+    return 0
+  fi
+  echo "Installing additional VOMSES ..."
+  [ -d /etc/vomses/ ] || mkdir -p /etc/vomses/
+  # VO: vo.indigo-datacloud.eu
+  sudo su - -c 'echo "\"vo.indigo-datacloud.eu\" \"voms01.ncg.ingrid.pt\" \"40101\" \"/C=PT/O=LIPCA/O=LIP/OU=Lisboa/CN=voms01.ncg.ingrid.pt\" \"vo.indigo-datacloud.eu\"" > /etc/vomses/indigo-voms01.ncg.ingrid.pt'
+  mkdir -p /etc/grid-security/vomsdir/vo.indigo-datacloud.eu
+  cat > /etc/grid-security/vomsdir/vo.indigo-datacloud.eu/voms01.ncg.ingrid.pt.lsc <<EOF
+/C=PT/O=LIPCA/O=LIP/OU=Lisboa/CN=voms01.ncg.ingrid.pt
+/C=PT/O=LIPCA/CN=LIP Certification Authority
+EOF
+  # VO: fedcloud.egi.eu
+  sudo su - -c 'echo "\"fedcloud.egi.eu\" \"voms1.grid.cesnet.cz\" \"15002\" \"/DC=org/DC=terena/DC=tcs/C=CZ/ST=Hlavni mesto Praha/L=Praha 6/O=CESNET/CN=voms1.grid.cesnet.cz\" \"fedcloud.egi.eu\"" > /etc/vomses/fedcloud.egi.eu-voms1.grid.cesnet.cz'
+  sudo su - -c 'echo "\"fedcloud.egi.eu\" \"voms2.grid.cesnet.cz\" \"15002\" \"/DC=org/DC=terena/DC=tcs/C=CZ/ST=Hlavni mesto Praha/L=Praha 6/O=CESNET/CN=voms2.grid.cesnet.cz\" \"fedcloud.egi.eu\"" > /etc/vomses/fedcloud.egi.eu-voms2.grid.cesnet.cz'
+  mkdir -p /etc/grid-security/vomsdir/fedcloud.egi.eu
+  cat > /etc/grid-security/vomsdir/fedcloud.egi.eu/voms1.grid.cesnet.cz.lsc <<EOF
+/DC=org/DC=terena/DC=tcs/C=CZ/ST=Hlavni mesto Praha/L=Praha 6/O=CESNET/CN=voms1.grid.cesnet.cz
+/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3
+EOF
+  cat > /etc/grid-security/vomsdir/fedcloud.egi.eu/voms2.grid.cesnet.cz.lsc << EOF
+/DC=org/DC=terena/DC=tcs/C=CZ/ST=Hlavni mesto Praha/L=Praha 6/O=CESNET/CN=voms2.grid.cesnet.cz
+/C=NL/ST=Noord-Holland/L=Amsterdam/O=TERENA/CN=TERENA eScience SSL CA 3
+EOF
+  # report to .fgSetup to track success    
+  get_ts
+  echo "$TS  vomses" >> $RUNDIR/.fgSetup
+}
+
+# post installation steps
+postinstall_occi() {
+  # go back to the CURRDIR
+  cd $CURRDIR
+  # Final message
+  echo "Installation script accomplished"
+  echo ""
+  # report to .fgSetup to track success  
+  get_ts
+  echo "$TS  occiend" >> $RUNDIR/.fgSetup
+  return 0
+}
+
+# uninstall script
+occi_uninstall() {
+  echo "WARNING: Sorry, OCCI uninstallation not supported yet"
+}
+
+##
+## Script execution
+##
+if [ "${1}" != "" ]; then
+  if [ "${1}" = "-u" ]; then
+    echo "Uninstalling OCCI ..."
+    occi_uninstall
+  elif [ "${1}" = "-h" -o "${1}" = "--help" ]; then
+    SNAME=$(basename $0)
+    echo "Usage: ${SNAME} [-u] [-h|--help]"
+    echo "Execute without arguments to install OCCI client"
+    echo "Use -h or --help to show this page"
+    echo ""
+    echo "This script will install OCCI client"
+    exit 0
+  else
+    echo "FATAL: Unrecognized option: \"${1}\""
+    exit 1
+  fi
+else
+  preinstall_occi   && \
+  install_gsi       && \
+  install_occi      && \
+  install_vomses    && \
+  postinstall_occi
+fi
+
+exit 0
---
+++ fgw_install_scripts/setup_config.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+#
+# Setup configuration script for any FutureGateway setup script
+#
+# 01.10.2015 - riccardo.bruno@ct.infn.it
+#
+# This file contains common variables for setup_* scripts it may be used to override 
+# values defined inside setup settings
+
+#
+# Setup configuration variables
+#
+# Uncomment and change one ot these values to override default settings
+# specified inside each setup_* scripts
+
+#
+# Common values; FG user, FG user directory, FG file repo, FG home dir, FG environment
+#
+FGUSER=futuregateway                                 # User owning FutureGateway files
+FGHOME=$HOME                                    # This script could be executed as root; specify FG home here
+FGREPO=$FGHOME/FGRepo                           # Files could be cached into this repo directory
+FGLOCATION=$FGHOME/FutureGateway                # Location of the FutureGateway installation
+FGENV=$FGLOCATION/setenv.sh                     # FutureGateway environment variables
+
+#
+# setup_FGPortal.sh
+#
+TOMCATUSR=tomcat                              # TOMCAT username
+TOMCATPAS=2e81a5ee                              # TOMCAT password
+SKIP_LIFERAY=0                                      # 0 - Installs Liferay
+LIFERAY_VER=7                                       # Specify here the Liferay portal version: 6 or 7 (default)
+LIFERAY_SDK_ON=1                                    # 0 - SDK will be not installed
+LIFERAY_SDK_LOCATION=$FGLOCATION                   # Liferay SDK will be placed here
+ANT_ON=0                                            # 0 - Ant will be not installed (valid only if LIFERAY_SDK is on)
+MAVEN_ON=0                                          # 0 - Maven will be not installed (valid only if LIFERAY_SDK is on)
+STARTUP_SYSTEM=1                                    # 0 - The portlal will be not initialized (unused yet)
+TIMEZONE=$(date +%Z)                               # Set portal timezone as system timezone (portal should operate at UTC)
+SETUPDB=1                                           # 1 - Initialize Liferay DB
+# Below MYSQL settings...                           # !!! WARNING enabling this flag
+MYSQL_HOST=localhost                                # any existing DB will be dropped
+MYSQL_PORT=3306
+MYSQL_USER=lportal
+MYSQL_PASS=lportal
+MYSQL_DBNM=lportal
+MYSQL_ROOT=root
+MYSQL_RPAS=
+
+#
+# setup_JSAGA.sh
+#
+JSAGA_LOCATION=$FGHOME/FutureGateway              # Liferay SDK will be placed here
+
+#
+# setup_OCCI.sh
+#
+USEFEDCLOUD=1                                      # Set to 1 for FedCloud setup script
+
+#
+# setup_GridEngine.sh
+#
+GEDIR=$FGLOCATION/GridEngine
+GELOG=$GEDIR/log
+GELIB=$GEDIR/lib
+SETUPUTDB=1                                         # 1 - Initialize UsersTracking DB
+SETUPGRIDENGINEDAEMON=1                             # 1 - Configures GRIDENGINE Daemon
+RUNDIR=$FGHOME                                     # Normally placed at 
+GEMYSQL_HOST=localhost                              # Any existing DB will be dropped
+GEMYSQL_PORT=3306
+GEMYSQL_USER=tracking_user
+GEMYSQL_PASS=usertracking
+GEMYSQL_DBNM=userstracking
+
+#
+# Determine OS installer
+#
+BREW=$(which brew >/dev/null 2>/dev/null)
+APTGET=$(which apt-get 2>/dev/null)
+YUM=$(which yum 2>/dev/null)
+
+# Function that produces a timestamp
+get_ts() {
+ TS=$(date +%y%m%d%H%M%S)
+}
+
+# Function that retrieves a file from FGRepo or download it
+# from the web. The function takes three arguments:
+#   $1 - Source URL
+#   $2 - Destination path; (current dir if none; or only path to destination)
+#   $3 - Optional the name of the file (sometime source URL does not contain the name)
+# FGREPO directory exists, because created by the preinstall_fg
+get_file() {
+  if [ "${3}" != "" ]; then
+    FILENAME="${3}"
+  else
+    FILENAME=$(basename $1)
+  fi
+  if [ "${2}" != "" ]; then
+    DESTURL="${2}"
+  else
+    DESTURL=$(pwd)
+  fi
+  if [ -e "${FGREPO}/${FILENAME}" ]; then
+    # The file exists in the cache
+    echo "File ${FILENAME} exists in the cache" 
+    cp "${FGREPO}/${FILENAME}" $DESTURL/$FILENAME
+  else
+    echo "File ${FILENAME} not in cache; retrieving it from the web"
+    wget "${1}" -O $FGREPO/$FILENAME 2>/dev/null
+    RES=$?
+    if [ $RES -ne 0 ]; then
+      echo "FATAL: Unable to download from URL: ${1}"
+      rm -f $FGREPO/$FILENAME
+      exit 1
+    fi 
+    cp "${FGREPO}/${FILENAME}" $DESTURL/$FILENAME
+  fi
+}
+
+#
+# Function that replace the 1st matching occurrence of
+# a pattern with a given line into the specified filename
+#  $1 # File to change
+#  $2 # Matching pattern that identifies the line
+#  $3 # New line content
+#  $4 # Optionally specify a suffix to keep a safe copy
+replace_line() {
+  file_name=$1   # File to change
+  pattern=$2     # Matching pattern that identifies the line
+  new_line=$3    # New line content
+  keep_suffix=$4 # Optionally specify a suffix to keep a safe copy
+
+  if [ "$file_name" != "" -a -f $file_name -a "$pattern" != "" ]; then
+      TMP=$(mktemp)
+      cp $file_name $TMP
+      if [ "$keep_suffix" != "" ]; then # keep a copy of replaced file
+          cp $file_name $file_name"_"$keep_suffix
+      fi
+      MATCHING_LINE=$(cat $TMP | grep -n "$pattern" | head -n 1 | awk -F':' '{ print $1 }' | xargs echo)
+      if [ "$MATCHING_LINE" != "" ]; then
+          cat $TMP | head -n $((MATCHING_LINE-1)) > $file_name
+          printf "$new_line\n" >> $file_name 
+          cat $TMP | tail -n +$((MATCHING_LINE+1)) >> $file_name
+      else
+          echo "WARNING: Did not find '"$pattern"' in file: '"$file_name"'"
+      fi
+      rm -f $TMP
+  else
+      echo "You must provide an existing filename and a valid pattern"
+      return 1
+  fi
+}
